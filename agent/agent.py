d4656d22685b4ca5abb3daed54da6b019ca4d3f9
Enoch2090 <gyc990926@gmail.com>
2021-05-06 15:44:21 +0800

    0506 Update

--- agent/agent.py
+++ agent/agent.py
@@ -18,11 +18,13 @@ class TestAgent():
     def __init__(self):
         self.now_phase = {}
         self.green_sec = 40
-        self.red_sec = 5
-        self.max_phase = 4
+        self.max_phase = 8
         self.last_change_step = {}
         self.agent_list = []
         self.phase_passablelane = {}
+        self.intersections = {}
+        self.roads = {}
+        self.agents = {}
     ################################
     # don't modify this function.
     # agent_list is a list of agent_id
@@ -30,6 +32,28 @@ class TestAgent():
         self.agent_list = agent_list
         self.now_phase = dict.fromkeys(self.agent_list,1)
         self.last_change_step = dict.fromkeys(self.agent_list,0)
+
+    # intersections[key_id] = {
+    #     'have_signal': bool,
+    #     'end_roads': list of road_id. Roads that end at this intersection. The order is random.
+    #     'start_roads': list of road_id. Roads that start at this intersection. The order is random.
+    #     'lanes': list, contains the lane_id in. The order is explained in Docs.
+    # }
+    # roads[road_id] = {
+    #     'start_inter':int. Start intersection_id.
+    #     'end_inter':int. End intersection_id.
+    #     'length': float. Road length.
+    #     'speed_limit': float. Road speed limit.
+    #     'num_lanes': int. Number of lanes in this road.
+    #     'inverse_road':  Road_id of inverse_road.
+    #     'lanes': dict. roads[road_id]['lanes'][lane_id] = list of 3 int value. Contains the Steerability of lanes.
+    #               lane_id is road_id*100 + 0/1/2... For example, if road 9 have 3 lanes, then their id are 900, 901, 902
+    # }
+    # agents[agent_id] = list of length 8. contains the inroad0_id, inroad1_id, inroad2_id,inroad3_id, outroad0_id, outroad1_id, outroad2_id, outroad3_id
+    def load_roadnet(self,intersections, roads, agents):
+        self.intersections = intersections
+        self.roads = roads
+        self.agents = agents
     ################################
 
 
@@ -68,6 +92,7 @@ class TestAgent():
 
 
             actions[agent] = self.now_phase[agent]
+        # print(self.intersections,self.roads,self.agents)
         return actions
 
 scenario_dirs = [
